56c61d02b8bede8d0d6e5600ad2a49810811f26d
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-26 23:24:55 +0600

    explore javaScript Array method

---
+++ array.js
@@ -0,0 +1,89 @@
+const numbers = [52, 66, 55, 48, 96];
+
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+// const x = numbers[2];
+
+// console.log(x);
+// console.log(numbers);
+
+// numbers.push(100);
+// numbers.pop();
+// numbers.pop();
+// numbers.pop();
+// const x = numbers.pop();
+
+// console.log(numbers);
+// console.log(x);
+
+// contact Array
+
+// const fruits = ["am,jaam, kathal,lichu,kola"];
+// const flowers = ["lotas,sunflower,bely,"];
+// const mixed = [fruits.concat(flowers)];
+// const mixed = [flowers.concat(flowers)];
+// const mixed = flowers.concat(fruits);
+
+// console.log(fruits);
+// console.log(flowers);
+// console.log(mixed);
+
+// console.log(mixed[4]);
+// console.log(mixed[0]);
+
+// const fruits = ["am", "jaam", "kathal", "lichu", "kola"];
+// const flowers = ["lotas", "sunflower", "bely"];
+
+// const mixed = flowers.concat(fruits);
+// const mixed = flowers.concat(fruits);
+
+// console.log(mixed[4]);
+
+// const find = mixed.indexOf("lichu");
+// const find = mixed.indexOf("tormuj");
+
+// console.log(find);
+
+// const flowers = ["lotas", "sunflower", "bely"];
+
+// console.log(flowers.includes("joba"));
+// console.log(flowers.includes("lotas"));
+
+// const fools = flowers.join();
+// const fools = flowers.join("/");
+// const fools = flowers.join("+");
+
+// console.log(fools);
+// console.log(typeof fools);
+// console.log(fools);
+
+// const string = "lotas,joba,beli,sunflower";
+// const convert = string.split("");
+// const convert = string.split(",");
+// const convert = string.split("a");
+
+// console.log(convert);
+// console.log(Array.isArray(convert));
+
+const flowers = [
+  "rojonigondha",
+  "Hasnahena",
+  "dalia",
+  "golap",
+  "surjomukhi",
+  "poddo",
+];
+
+// console.log(flowers.slice(1, 3));
+// console.log(flowers.slice(1, 4));
+
+// flowers.shift();
+// flowers.shift();
+
+// flowers.unshift("gada");
+// flowers.unshift("gada", "krishnochura");
+
+// flowers.unshift(([1], "kodom"));
+
+console.log(flowers);
